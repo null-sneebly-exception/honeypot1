cee66d44954ebe6baf3b200c03ace3d69ac32cee
Ubuntu <ubuntu@ip-172-31-25-44.ec2.internal>
2022-03-15 15:57:07 +0000

    first commit from aws, removed pycache files from repo and added public aws ip

--- honeypot_project/settings.py
+++ honeypot_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','52.201.246.164']
 
 SESSION_SAVE_EVERY_REQUEST = True
 
